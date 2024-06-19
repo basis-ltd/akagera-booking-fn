e9b5b5eb3e1f0c3a308284192ff2046a05e9bfff
Nishimwe Prince <princeelysee@gmail.com>
2024-06-19 16:59:26 +0200

    chore (env variables and loader)
    - adds visible loader
    - updates environment variables import

--- src/constants/environments.constants.ts
+++ src/constants/environments.constants.ts
@@ -1,4 +1,4 @@
 export const {
   VITE_APP_LOCAL_API_URL: localApiUrl,
-  VITE_APP_STAGING_API_URL: stagingApiUrl = 'akagera-booking.azurewebsites.net/api',
+  VITE_APP_STAGING_API_URL: stagingApiUrl,
 } = import.meta.env;
